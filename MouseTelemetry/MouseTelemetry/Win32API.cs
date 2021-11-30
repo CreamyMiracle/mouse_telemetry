d72e89f241bf7335b9b4ee930bec378e96df77aa
Juhana Rajala <juhana.rajala@vertex.fi>
2021-11-30 09:24:20 +0200

    Checkpoint

--- MouseTelemetry/MouseTelemetry/Win32API.cs
+++ MouseTelemetry/MouseTelemetry/Win32API.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace MouseTelemetry
 {
     public class Win32API
     {
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
         [StructLayout(LayoutKind.Sequential)]
         public class POINT
         {
@@ -29,5 +33,32 @@ namespace MouseTelemetry
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+
+
+        public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        public string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
     }
 }
